cd991f7d573945861cf5fde06a8db8c06ac250d8
NangTuong <nguyen.brad@outlook.com>
2022-05-22 23:48:04 -0400

    added javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// variables 
+var time = moment();
+var displayMoment = document.getElementById("currentDay");
+displayMoment.innerHTML = time.format("M-D-YYYY, H:MMA");
+var currentTime = moment().hours();
+
+
+//Change textarea background color based on time
+$(".hour").each(function() {
+
+    var currentTime = moment().hours()
+
+    if (parseInt($(this).data("time")) < currentTime) {
+          $(this).next().addClass("bg-secondary")
+    }
+    if (parseInt($(this).data("time")) == currentTime) {
+          $(this).next().addClass("bg-success")
+    }
+    if (parseInt($(this).data("time")) > currentTime) {
+          $(this).next().addClass("bg-danger")
+    }
+});
+
+// save user input to local storage
+$(".saveBtn").click (function() {
+    $(this).siblings("textarea").val()
+
+    var text = $(this).siblings("textarea").val()
+    //console.log(text)
+
+    localStorage.setItem("input", text)
+
+    localStorage.getItem("input")
+});
+
+// checkTime every 5 minutes
+setInterval(currentTime(), (1000 * 60) * 5);
